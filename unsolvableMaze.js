244f007666aaf17d56b065fc6a94604df2a3dcc1
G3rste <philippgerstenberg@web.de>
2023-12-23 17:35:37 +0100

    remove solutions

--- unsolvableMaze.js
+++ unsolvableMaze.js
@@ -1,8 +1,8 @@
 function unsolvableMaze(){
     return [["wall", "wall", "wall", "wall", "wall"],
-            ["wall", "floor", "floor", "floor", "wall"],
+            ["wall", "floor", "wall", "floor", "target"],
             ["wall", "floor", "wall", "floor", "wall"],
-            ["wall", "floor", "target", "floor", "wall"],
+            ["wall", "floor", "wall", "floor", "wall"],
             ["wall", "floor", "wall", "floor", "wall"],
             ["wall", "floor", "floor", "floor", "wall"],
             ["wall", "wall", "wall", "wall", "wall"]]
